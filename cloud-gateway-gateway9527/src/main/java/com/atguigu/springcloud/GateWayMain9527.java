6abbd60a17bf93278d99592f6df9c9ecb79a3208
FandySww <fa370@sina.com>
2020-09-20 12:18:12 +0800

    用idea提交

--- cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -1,8 +1,18 @@
 package com.atguigu.springcloud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
 
 /**
  * @auther zzyy
@@ -14,5 +24,38 @@ public class GateWayMain9527
 {
     public static void main(String[] args) {
             SpringApplication.run(GateWayMain9527.class, args);
+    }
+
+    /**
+     * @auther zzyy
+     * @create 2020-02-21 16:40
+     */
+    @Component
+    @Slf4j
+    public static class MyLogGateWayFilter implements GlobalFilter, Ordered
+    {
+
+        @Override
+        public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain)
+        {
+            log.info("***********come in MyLogGateWayFilter:  "+new Date());
+
+            String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+
+            if(uname == null)
+            {
+                log.info("*******用户名为null，非法用户，o(╥﹏╥)o");
+                exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+                return exchange.getResponse().setComplete();
+            }
+
+            return chain.filter(exchange);
+        }
+
+        @Override
+        public int getOrder()
+        {
+            return 0;
+        }
     }
 }
